7e8c616540eb2e42f0f43269cf5ce150d39604f2
Chivorix <danilocim98@gmail.com>
2024-09-29 18:45:47 +0200

    collision done

--- circleshape.py
+++ circleshape.py
@@ -18,4 +18,11 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
-        
+
+    def collision(self, target):               
+        distance = self.position.distance_to(target.position)
+        if distance <= (self.radius + target.radius):
+            return True
+        else:
+            return False        
+
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pygame
 from constants import *
 from player import Player
-
-
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -16,8 +15,13 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
     Player.containers = (updatable, drawable)
 
+    asteroid_spawner = AsteroidField()
     player = Player(PLAYER_X, PLAYER_Y)
 
     while True:
@@ -27,12 +31,15 @@ def main():
             obj.update(dt)
         for obj in drawable:
             obj.draw(screen)
+        for obj in asteroids:
+            if obj.collision(player):
+                print("Game Over!")
+                return 
 
         pygame.display.flip()           # double-buffer mechanic, this is basically a copy-paste function, rendering...
 
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
-                print(dt)
                 return                  # break the loop
 
         dt = clock.tick(60) / 1000      # The .tick method returns Delta-t in ms
